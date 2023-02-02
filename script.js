fdb56307b271e049ff90bc8bd8f2a1253f78f09e
ricardo03silva <rnsilva@DEV48.intranet.digitalsign.pt>
2023-02-02 12:59:04 +0000

    estrutura de dados

--- script.js
+++ script.js
@@ -5,20 +5,61 @@ function myFunction() {
     let minus = document.getElementById('counter-minus');
     let plus = document.getElementById('counter-plus');
     let count=0;
+    const eventos = [];
 
     updateCounter();
 
+    function formatDate(date) {
+        return [
+          padTo2Digits(date.getDate()),
+          padTo2Digits(date.getMonth() + 1),
+          date.getFullYear(),
+        ].join('/');
+    }
+      
+    function padTo2Digits(num) {
+        return num.toString().padStart(2, '0');
+    }
+
+    function createRecord(operation, oldcount, count, timestamp){
+        const record = new Object();
+        //insert data on object
+        record.operation = operation;
+        record.oldValue = oldcount;
+        record.newValue = count;
+        record.timestamp = timestamp;
+
+        eventos.push(record);
+        //str = JSON.stringify(record);
+        //console.log(str);
+    }
+
+
     plus.addEventListener("click",()=>{
+        let oldcount = count;
         count++;
+        let operation = 'plus';
+        timestamp = formatDate(new Date());
         updateCounter();
+        createRecord(operation, oldcount, count, timestamp);
     });
 
     minus.addEventListener("click",()=>{
+        let oldcount = count;
         count--;
+        let operation = 'minus';
+        timestamp = formatDate(new Date());
         updateCounter();
+        createRecord(operation, oldcount, count, timestamp);
     });
 
     function updateCounter(){
         result.innerHTML = count;
     };
+    
+    function printElem() {
+
+    }
+    
+    eventos.forEach(printElem);
 }
